c811e5e844c93872f1883e081d7f398993e7f731
Benhur CR <67530801+BenhurGG@users.noreply.github.com>
2022-06-02 08:50:49 -0500

    Add files via upload

---
+++ hospitalUCIManagement/src/main/java/pe/edu/upc/demo/HospitalUciManagementApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalUciManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalUciManagementApplication.class, args);
+	}
+
+}
